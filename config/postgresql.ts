a161a87e88e075ff18869b74d0daece61bf5f946
Z1ZA <sayaabdaziz1@gmail.com>
2024-02-15 13:31:40 +0700

    feat: add BE challenge 8

---
+++ config/postgresql.ts
@@ -0,0 +1,16 @@
+import knex from "knex";
+import dotenv from "dotenv";
+dotenv.config();
+
+const knexInstance = knex({
+  client: process.env.DB_CLIENT,
+  connection: {
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    port: Number(process.env.DB_PORT),
+  },
+});
+
+export default knexInstance;
---
+++ knexfile.ts
@@ -0,0 +1,62 @@
+import type { Knex } from "knex";
+import dotenv from "dotenv";
+
+dotenv.config();
+// Update with your config settings.
+
+const config: { [key: string]: Knex.Config } = {
+  development: {
+    client: process.env.DB_CLIENT,
+    connection: {
+      host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      port: Number(process.env.DB_PORT),
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+    seeds: {
+      directory: "./seeds",
+    },
+  },
+
+  staging: {
+    client: "postgresql",
+    connection: {
+      database: "binar-car-rental",
+      user: "postgres",
+      password: "postgres",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
+
+  production: {
+    client: "postgresql",
+    connection: {
+      database: "binar-car-rental",
+      user: "postgres",
+      password: "postgres",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
+};
+
+module.exports = config;
